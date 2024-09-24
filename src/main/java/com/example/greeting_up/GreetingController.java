60579344c9805ac9f57cbb76aa7553b7ad9f1070
Evgeniy Pusser <36860378+EvgeniyPusser@users.noreply.github.com>
2024-09-24 22:01:39 +0300

    It was really trash!!!

---
+++ src/main/java/com/example/greeting_up/GreetingController.java
@@ -0,0 +1,17 @@
+package com.example.greeting_up;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+    @Controller
+    public class GreetingController{
+
+        @GetMapping("/")
+        public String greeting(Model model) {
+            model.addAttribute("message", "Добро пожаловать!");
+            return "greeting";
+        }
+    }
+
+
---
+++ src/test/java/com/example/greeting_up/GreetingUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greeting_up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
